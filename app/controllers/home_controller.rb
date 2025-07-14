a1382616e29eb75d9cf25f6ee377f963d0f23164
Curt Bercht Neto <bercht@live.com>
2025-07-14 19:03:12 -0300

    home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,16 @@ class HomeController < ApplicationController
     @featured_properties = Property.featured
                                   .includes(:neighborhood)
                                   .order(created_at: :desc)
+    
+    # Se não tiver 3 imóveis em destaque, completar com os mais recentes
+    if @featured_properties.count < 4
+      recent_properties = Property.includes(:neighborhood)
+                                 .where.not(id: @featured_properties.pluck(:id))
+                                 .order(created_at: :desc)
+                                 .limit(4 - @featured_properties.count)
+      @featured_properties = (@featured_properties + recent_properties).first(4)
+    end
+    
     @neighborhoods = Neighborhood.all
   end
 end

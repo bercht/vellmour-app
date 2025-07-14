b704e678f8039c0326f0b56904bb9eb55ef0a9a5
Curt Bercht Neto <bercht@live.com>
2025-07-14 14:06:19 -0300

    home dividida

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @featured_properties = Property.includes(:neighborhood)
+                                  .where.not(images: { id: nil })
+                                  .joins(:images_attachments)
+                                  .limit(3)
+                                  .order(created_at: :desc)
+    @neighborhoods = Neighborhood.all
   end
 end
